875f04597ada28159f99831061f9b3131d65cd7f
Jonas kamsker <jonaskamsker@lints.at>
2019-03-30 01:48:18 +0100

    Potentially Increased performance(http2.0)&Fixed parsing bug

---
+++ src/OpenPr0gramm/Http2CustomHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace OpenPr0gramm
+{
+    public class Http2CustomHandler : WinHttpHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
+        {
+            request.Version = new Version("2.0");
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
