971e775ce0fa06aedbdea0bce175b92bceeda91c
stovflco <84211585+stovflco@users.noreply.github.com>
2021-05-15 22:00:16 +0200

    #29 Neues SyncItem-Model, Converter, SyncActions (#30)
    Co-authored-by: Hendrik Heinle <hendrik.heinle@fluxel.de>

---
+++ src/OpenPr0gramm/Model/SyncAction.cs
@@ -0,0 +1,27 @@
+﻿namespace OpenPr0gramm.Model
+{
+    public enum SyncAction
+    {
+        VoteItemDown = 1,
+        VoteItemNeutral = 2,
+        VoteItemUp = 3,
+        VoteCommentDown = 4,
+        VoteCommentNeutral = 5,
+        VoteCommentUp = 6,
+        VoteTagDown = 7,
+        VoteTagNeutral = 8,
+        VoteTagUp = 9,
+
+        FollowStateFollow1 = 12,
+        FollowStateNone = 13,
+        FollowStateSubscribed = 14,
+        FollowStateFollow2 = 15,
+
+        CommentFav = 16,
+        CommentUnFav = 17,
+
+        ItemUnCollect = 18,
+        ItemCollect = 19,
+        CollectionId = 20
+    }
+}
